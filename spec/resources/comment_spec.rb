b0d95f886f9d9e16f3a85df86c9341b18b4922db
Lorenzo Planas <lorenzo@qindio.com>
2014-04-22 18:40:45 +0200

    Add spec for Comment.repository=

--- spec/resources/comment_spec.rb
+++ spec/resources/comment_spec.rb
@@ -5,10 +5,12 @@ require_relative '../../resources/comment'
 include Blabber
 
 describe '#repository=' do
-  it 'lalas' do
+  it 'accepts a single object as repository for members and collections' do
     Comment.repository = "memory"
     Comment.repository.must_equal "memory"
+  end
 
+  it 'accepts tuple of repositories for members and collections' do
     Comment.repository = ["member", "collection"]
     Comment.repository(:member).must_equal "member"
     Comment.repository(:collection).must_equal "collection"
